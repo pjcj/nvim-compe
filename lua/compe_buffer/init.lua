5d5117d1be361c0b47fe9f51bde06d152a8311f1
Paul Johnson <paul@pjcj.net>
2021-07-21 18:47:55 +0200

    Commit

--- lua/compe_buffer/init.lua
+++ lua/compe_buffer/init.lua
@@ -64,6 +64,7 @@ end
 --- _get_bufs
 function Source._get_buffers(self)
   local bufs = {}
+
   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
     if vim.api.nvim_buf_is_loaded(buf) and vim.fn.buflisted(buf) == 1 then
       bufs[buf] = true
