57860440548d861ead16817014bc2ab83ff74426
Paul Johnson <paul@pjcj.net>
2021-07-22 00:25:00 +0200

    Name 2

--- lua/compe_buffer/init.lua
+++ lua/compe_buffer/init.lua
@@ -68,7 +68,7 @@ function Source._get_buffers(self)
   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
     -- if vim.api.nvim_buf_is_loaded(buf) then
       bufs[buf] = true
-      print(vim.api.nvim_buf_get_name())
+      print(vim.api.nvim_buf_get_name(buf))
     -- end
   end
 
