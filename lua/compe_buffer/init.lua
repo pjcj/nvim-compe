759ac648c281560bf29cad493db8a045a31daed9
Paul Johnson <paul@pjcj.net>
2021-07-22 00:06:16 +0200

    Print

--- lua/compe_buffer/init.lua
+++ lua/compe_buffer/init.lua
@@ -65,9 +65,10 @@ end
 function Source._get_buffers(self)
   local bufs = {}
 
-  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+  for i, buf in ipairs(vim.api.nvim_list_bufs()) do
     -- if vim.api.nvim_buf_is_loaded(buf) then
       bufs[buf] = true
+      print(i)
     -- end
   end
 
